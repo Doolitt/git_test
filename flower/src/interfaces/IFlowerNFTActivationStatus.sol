aee3e8e3c1cc9f76545a080bfdb6cdc6d5034854
Royal Doolittle <50808473+Doolitt@users.noreply.github.com>
2026-08-31 00:26:23 -0400

    Type the FLOWER NFT activation status interface

--- flower/src/interfaces/IFlowerNFTActivationStatus.sol
+++ flower/src/interfaces/IFlowerNFTActivationStatus.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {IActivationTransferHook} from "./IActivationTransferHook.sol";
+
 interface IFlowerNFTActivationStatus {
-    function activationHook() external view returns (address);
+    function activationHook() external view returns (IActivationTransferHook);
 }
