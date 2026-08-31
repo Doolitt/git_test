e720dda0ac44f7e807879d6a5a3073ce120706fe
Royal Doolittle <50808473+Doolitt@users.noreply.github.com>
2026-08-31 00:06:56 -0400

    Harden activation accounting and simplify curve math

--- flower/src/ActivationManager.sol
+++ flower/src/ActivationManager.sol
@@ -12,10 +12,7 @@ import {UD60x18, ud} from "@prb/math/src/UD60x18.sol";
 import {IActivationTransferHook} from "./interfaces/IActivationTransferHook.sol";
 import {IWeightProvider} from "./interfaces/IWeightProvider.sol";
 import {IRewardDistributor} from "./interfaces/IRewardDistributor.sol";
-
-interface IFLOWER is IERC20 {
-    function burnFrom(address account, uint256 amount) external;
-}
+import {IFLOWER} from "./interfaces/IFLOWER.sol";
 
 /// @title ActivationManager
 /// @notice Escrows temporary FLOWER locks and tracks permanent FLOWER burns by NFT.
@@ -27,16 +24,10 @@ interface IFLOWER is IERC20 {
 /// or transfers the NFT. This avoids stale global-weight accounting from automatic
 /// expiries. If the desired economic rule is "boost automatically ends at expiry",
 /// implement epoch-aligned expiries/checkpointing before production deployment.
-contract ActivationManager is
-    IActivationTransferHook,
-    IWeightProvider,
-    Ownable2Step,
-    ReentrancyGuard
-{
+contract ActivationManager is IActivationTransferHook, IWeightProvider, Ownable2Step, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     uint256 public constant WAD = 1e18;
-
     uint256 public constant ACTIVATION_FLOOR = 50_000_000 ether;
     uint256 public constant BASE_WEIGHT = 0.25e18;
     uint256 public constant HILL_MIDPOINT = 300_000_000 ether;
@@ -47,8 +38,8 @@ contract ActivationManager is
     uint256 public constant BURN_COEFFICIENT = 0.30e18;
     uint256 public constant BURN_SCALE = 20_000_000 ether;
 
-    IERC721 public immutable nft;
-    IFLOWER public immutable flower;
+    IERC721 public immutable NFT;
+    IFLOWER public immutable FLOWER_TOKEN;
 
     IRewardDistributor public rewardDistributor;
     uint256 public override totalWeight;
@@ -85,6 +76,8 @@ contract ActivationManager is
     error RewardDistributorAlreadySet();
     error ZeroAddress();
     error DetachedLockUnavailable();
+    error LockAmountOverflow();
+    error TimestampOverflow();
 
     event RewardDistributorSet(address indexed distributor);
     event Activated(
@@ -115,8 +108,8 @@ contract ActivationManager is
 
     constructor(address initialOwner, IERC721 nft_, IFLOWER flower_) Ownable(initialOwner) {
         if (address(nft_) == address(0) || address(flower_) == address(0)) revert ZeroAddress();
-        nft = nft_;
-        flower = flower_;
+        NFT = nft_;
+        FLOWER_TOKEN = flower_;
     }
 
     function setRewardDistributor(IRewardDistributor distributor) external onlyOwner {
@@ -126,76 +119,77 @@ contract ActivationManager is
         emit RewardDistributorSet(address(distributor));
     }
 
-    function activate(uint256 tokenId, uint256 lockAmount, uint16 durationDays)
-        external
-        nonReentrant
-    {
-        if (nft.ownerOf(tokenId) != msg.sender) revert NotNFTOwner();
+    function activate(uint256 tokenId, uint256 lockAmount, uint16 durationDays) external nonReentrant {
+        if (NFT.ownerOf(tokenId) != msg.sender) revert NotNFTOwner();
         if (positions[tokenId].activator != address(0)) revert PositionExists();
         if (lockAmount < ACTIVATION_FLOOR) revert BelowActivationFloor();
 
-        uint64 unlockAt = uint64(block.timestamp + _durationSeconds(durationDays));
-        IERC20(address(flower)).safeTransferFrom(msg.sender, address(this), lockAmount);
-
+        uint128 storedLock = _toUint128(lockAmount);
+        uint64 unlockAt = _unlockTimestamp(durationDays);
         uint256 newWeight = computeWeight(lockAmount, permanentBurned[tokenId], durationDays);
 
         positions[tokenId] = Position({
             activator: msg.sender,
-            locked: uint128(lockAmount),
+            locked: storedLock,
             unlockAt: unlockAt,
             durationDays: durationDays,
             weight: newWeight
         });
-
         totalWeight += newWeight;
-        _notifyWeightChange(tokenId, msg.sender, 0, newWeight);
 
         emit Activated(tokenId, msg.sender, lockAmount, durationDays, unlockAt, newWeight);
+        _notifyWeightChange(tokenId, msg.sender, 0, newWeight);
+
+        IERC20(address(FLOWER_TOKEN)).safeTransferFrom(msg.sender, address(this), lockAmount);
     }
 
     function increaseLock(uint256 tokenId, uint256 additionalAmount) external nonReentrant {
         if (additionalAmount == 0) revert InvalidAmount();
-        if (nft.ownerOf(tokenId) != msg.sender) revert NotNFTOwner();
+        if (NFT.ownerOf(tokenId) != msg.sender) revert NotNFTOwner();
 
         Position storage p = positions[tokenId];
         if (p.activator == address(0)) revert NoPosition();
         if (p.activator != msg.sender) revert NotActivator();
 
-        IERC20(address(flower)).safeTransferFrom(msg.sender, address(this), additionalAmount);
-
         uint256 oldWeight = p.weight;
         uint256 newLocked = uint256(p.locked) + additionalAmount;
         uint256 newWeight = computeWeight(newLocked, permanentBurned[tokenId], p.durationDays);
 
-        p.locked = uint128(newLocked);
+        p.locked = _toUint128(newLocked);
         p.weight = newWeight;
-
         _replaceTotalWeight(oldWeight, newWeight);
-        _notifyWeightChange(tokenId, msg.sender, oldWeight, newWeight);
 
         emit LockIncreased(tokenId, additionalAmount, newLocked, newWeight);
+        _notifyWeightChange(tokenId, msg.sender, oldWeight, newWeight);
+
+        IERC20(address(FLOWER_TOKEN)).safeTransferFrom(msg.sender, address(this), additionalAmount);
     }
 
     function burnForDevelopment(uint256 tokenId, uint256 amount) external nonReentrant {
         if (amount == 0) revert InvalidAmount();
-        if (nft.ownerOf(tokenId) != msg.sender) revert NotNFTOwner();
+        if (NFT.ownerOf(tokenId) != msg.sender) revert NotNFTOwner();
 
-        flower.burnFrom(msg.sender, amount);
-        permanentBurned[tokenId] += amount;
+        uint256 cumulativeBurn = permanentBurned[tokenId] + amount;
+        permanentBurned[tokenId] = cumulativeBurn;
 
         Position storage p = positions[tokenId];
-        uint256 newWeight = 0;
+        uint256 newWeight;
+        uint256 oldWeight;
 
         if (p.activator != address(0)) {
-            uint256 oldWeight = p.weight;
-            newWeight = computeWeight(uint256(p.locked), permanentBurned[tokenId], p.durationDays);
+            oldWeight = p.weight;
+            newWeight = computeWeight(uint256(p.locked), cumulativeBurn, p.durationDays);
             p.weight = newWeight;
-
             _replaceTotalWeight(oldWeight, newWeight);
+        }
+
+        emit PermanentBurn(tokenId, msg.sender, amount, cumulativeBurn, newWeight);
+
+        if (p.activator != address(0)) {
             _notifyWeightChange(tokenId, msg.sender, oldWeight, newWeight);
         }
 
-        emit PermanentBurn(tokenId, msg.sender, amount, permanentBurned[tokenId], newWeight);
+        FLOWER_TOKEN.burnFrom(msg.sender, amount);
     }
 
     function withdraw(uint256 tokenId) external nonReentrant {
@@ -206,18 +200,15 @@ contract ActivationManager is
 
         delete positions[tokenId];
         totalWeight -= p.weight;
+
+        emit PositionWithdrawn(tokenId, msg.sender, uint256(p.locked));
         _notifyWeightChange(tokenId, msg.sender, p.weight, 0);
 
-        IERC20(address(flower)).safeTransfer(msg.sender, uint256(p.locked));
-        emit PositionWithdrawn(tokenId, msg.sender, uint256(p.locked));
+        IERC20(address(FLOWER_TOKEN)).safeTransfer(msg.sender, uint256(p.locked));
     }
 
-    function onNftTransfer(uint256 tokenId, address from, address)
-        external
-        override
-        nonReentrant
-    {
-        if (msg.sender != address(nft)) revert NotFlowerNFT();
+    function onNftTransfer(uint256 tokenId, address from, address) external override nonReentrant {
+        if (msg.sender != address(NFT)) revert NotFlowerNFT();
 
         Position memory p = positions[tokenId];
         if (p.activator == address(0)) return;
@@ -225,7 +216,6 @@ contract ActivationManager is
 
         delete positions[tokenId];
         totalWeight -= p.weight;
-        _notifyWeightChange(tokenId, from, p.weight, 0);
 
         uint256 detachedId = nextDetachedLockId++;
         detachedLocks[detachedId] = DetachedLock({
@@ -236,20 +226,19 @@ contract ActivationManager is
         });
 
         emit PositionDetached(tokenId, detachedId, from, uint256(p.locked), p.unlockAt);
+        _notifyWeightChange(tokenId, from, p.weight, 0);
     }
 
     function claimDetachedLock(uint256 detachedId) external nonReentrant {
         DetachedLock storage d = detachedLocks[detachedId];
-        if (d.beneficiary != msg.sender || d.claimed || d.amount == 0) {
-            revert DetachedLockUnavailable();
-        }
+        if (d.beneficiary != msg.sender || d.claimed || d.amount == 0) revert DetachedLockUnavailable();
         if (block.timestamp < d.unlockAt) revert LockStillCommitted();
 
         d.claimed = true;
         uint256 amount = uint256(d.amount);
-        IERC20(address(flower)).safeTransfer(msg.sender, amount);
 
         emit DetachedLockClaimed(detachedId, msg.sender, amount);
+        IERC20(address(FLOWER_TOKEN)).safeTransfer(msg.sender, amount);
     }
 
     function weightOf(uint256 tokenId) external view override returns (uint256) {
@@ -263,36 +252,31 @@ contract ActivationManager is
     {
         if (lockAmount < ACTIVATION_FLOOR) return 0;
 
-        UD60x18 total = ud(BASE_WEIGHT);
-        uint256 x = lockAmount - ACTIVATION_FLOOR;
+        uint256 durationMultiplier = _durationMultiplier(durationDays);
+        uint256 lockBoost = _lockBoost(lockAmount - ACTIVATION_FLOOR, durationMultiplier);
+        uint256 burnBoost = _burnBoost(burnAmount);
 
-        if (x != 0) {
-            UD60x18 xFP = ud(x);
-            UD60x18 kFP = ud(HILL_MIDPOINT);
-            UD60x18 powerFP = ud(HILL_POWER);
+        return BASE_WEIGHT + lockBoost + burnBoost;
+    }
 
-            UD60x18 xPow = xFP.pow(powerFP);
-            UD60x18 kPow = kFP.pow(powerFP);
-            UD60x18 coreFraction = xPow.div(xPow + kPow);
-            UD60x18 core = ud(HILL_MAX_BOOST).mul(coreFraction);
+    function _lockBoost(uint256 x, uint256 durationMultiplier) internal pure returns (uint256) {
+        if (x == 0) return 0;
 
-            UD60x18 tailRatio = xFP.div(ud(TAIL_SCALE));
-            UD60x18 tail = ud(TAIL_COEFFICIENT).mul((ud(WAD) + tailRatio).ln());
+        UD60x18 xFp = ud(x);
+        UD60x18 xPow = xFp.pow(ud(HILL_POWER));
+        UD60x18 kPow = ud(HILL_MIDPOINT).pow(ud(HILL_POWER));
 
-            UD60x18 lockComponent = (core + tail).mul(ud(_durationMultiplier(durationDays)));
-            total = total + lockComponent;
-        } else {
-            _durationMultiplier(durationDays);
-        }
+        UD60x18 core = ud(HILL_MAX_BOOST).mul(xPow.div(xPow + kPow));
+        UD60x18 tail = ud(TAIL_COEFFICIENT).mul((ud(WAD) + xFp.div(ud(TAIL_SCALE))).ln());
 
-        if (burnAmount != 0) {
-            UD60x18 burnRatio = ud(burnAmount).div(ud(BURN_SCALE));
-            UD60x18 burnComponent =
-                ud(BURN_COEFFICIENT).mul((ud(WAD) + burnRatio).ln());
-            total = total + burnComponent;
-        }
+        return (core + tail).mul(ud(durationMultiplier)).unwrap();
+    }
 
-        return total.unwrap();
+    function _burnBoost(uint256 burnAmount) internal pure returns (uint256) {
+        if (burnAmount == 0) return 0;
+
+        UD60x18 burnRatio = ud(burnAmount).div(ud(BURN_SCALE));
+        return ud(BURN_COEFFICIENT).mul((ud(WAD) + burnRatio).ln()).unwrap();
     }
 
     function _durationMultiplier(uint16 durationDays) internal pure returns (uint256) {
@@ -303,9 +287,16 @@ contract ActivationManager is
         revert InvalidDuration();
     }
 
-    function _durationSeconds(uint16 durationDays) internal pure returns (uint256) {
+    function _unlockTimestamp(uint16 durationDays) internal view returns (uint64) {
         _durationMultiplier(durationDays);
-        return uint256(durationDays) * 1 days;
+        uint256 unlockAt = block.timestamp + uint256(durationDays) * 1 days;
+        if (unlockAt > type(uint64).max) revert TimestampOverflow();
+        return uint64(unlockAt);
+    }
+
+    function _toUint128(uint256 amount) internal pure returns (uint128) {
+        if (amount > type(uint128).max) revert LockAmountOverflow();
+        return uint128(amount);
     }
 
     function _replaceTotalWeight(uint256 oldWeight, uint256 newWeight) internal {
